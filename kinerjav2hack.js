b0d2ce35302c1d8ea21791b831f22254b99faf2f
hisayuki <syuaibsyuaib@gmail.com>
2022-07-14 12:53:45 +0700

    stable 1.0.1

--- kinerjav2hack.js
+++ kinerjav2hack.js
@@ -1,4 +1,4 @@
-//$('body').append('<script src="https://cdn.jsdelivr.net/gh/syuaibsyuaib/kinerjav2hack@v1.0.1b/kinerjav2hack.js"></script>')
+//$('body').append('<script src="https://cdn.jsdelivr.net/gh/syuaibsyuaib/kinerjav2hack@v1.0.1/kinerjav2hack.js"></script>')
 
 /*
 
