360a1f6714d21b7eb49e7b2d9972f0e138eb281d
hisayuki <syuaibsyuaib@gmail.com>
2022-07-20 05:03:38 +0700

    1.0.3aa

--- kinerjav2hack.js
+++ kinerjav2hack.js
@@ -86,6 +86,7 @@ let proto = location.protocol == 'http:' ? 'http:' : 'https:'
           return res.json()
         })
         .then(resp => {
+          alert(resp)
           console.log(resp)
           nipStafSekolah = resp[0]
           klasifikasi = resp[1]
